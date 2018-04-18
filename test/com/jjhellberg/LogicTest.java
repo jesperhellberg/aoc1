a4e2ab3aefb36b81276a0ae436d34d6e6b86a7c6
jesper <jj.hellberg@gmail.com>
2018-04-18 21:31:37 +0200

    Added input, removed Intellij specific files

--- test/com/jjhellberg/LogicTest.java
+++ test/com/jjhellberg/LogicTest.java
@@ -1,8 +1,8 @@
 package com.jjhellberg;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
 
 class LogicTest {
 
